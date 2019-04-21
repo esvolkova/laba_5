5b2f5a1f76779cba5c5c8ba13c58ea043551fd10
esvolkova <48125195+esvolkova@users.noreply.github.com>
2019-04-21 22:05:37 +0300

    Add files via upload

---
+++ laba_5_file_config.py
@@ -0,0 +1,4 @@
+from onvif import ONVIFCamera
+mycam42 = ONVIFCamera('192.168.15.42', 80, 'admin', 'Supervisor')
+mycam43 = ONVIFCamera('192.168.15.43', 80, 'admin', 'Supervisor')
+#другие камеры без доступа
